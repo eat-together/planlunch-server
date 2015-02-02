4311e320613e244f0a2d1a0e7f34033722a56faa
pogopaule <pogopaule@gmx.de>
2015-02-02 18:18:30 +0100

    remove unused methods

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,19 +1,4 @@
 class AppointmentsController < ApplicationController
-  before_action :set_appointment, only: [:show, :update, :destroy]
-
-  # GET /appointments
-  # GET /appointments.json
-  def index
-    @appointments = Appointment.all
-
-    render json: @appointments
-  end
-
-  # GET /appointments/1
-  # GET /appointments/1.json
-  def show
-    render json: @appointment
-  end
 
   def create
     date = Time.zone.parse(appointment_params[:date])
@@ -29,33 +14,9 @@ class AppointmentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /appointments/1
-  # PATCH/PUT /appointments/1.json
-  def update
-    @appointment = Appointment.find(params[:id])
-
-    if @appointment.update(appointment_params)
-      head :no_content
-    else
-      render json: @appointment.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /appointments/1
-  # DELETE /appointments/1.json
-  def destroy
-    @appointment.destroy
-
-    head :no_content
-  end
-
   private
 
-    def set_appointment
-      @appointment = Appointment.find(params[:id])
-    end
-
-    def appointment_params
-      params.require(:appointment).permit(:user_id, :place_id, :date)
-    end
+  def appointment_params
+    params.require(:appointment).permit(:user_id, :place_id, :date)
+  end
 end
