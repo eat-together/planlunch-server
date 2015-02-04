0ecf635b7d0ee2784937841c0fba13a0fa98b354
pogopaule <pogopaule@gmx.de>
2015-02-04 22:03:08 +0100

    add index route

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,28 @@
 class AppointmentsController < ApplicationController
 
+  def index
+    today = Time.zone.now.strftime("%Y-%m-%d")
+
+    # TODO does its job but looks horrible, refactor!
+    todays_appointments = Appointment.where(date: today)
+    grouped_by_place = todays_appointments.group_by{|a| a.place_id}
+    grouped_by_place.each_pair do |place_id, appointments|
+      grouped_by_place[place_id] = appointments.group_by{|app| app.time}
+      grouped_by_place[place_id].each_pair do |time, apps|
+        grouped_by_place[place_id][time] = apps.map{|app| app.user_id}
+      end
+    end
+    result = []
+    grouped_by_place.each_pair do |k,v|
+      result << {
+        place_id: k,
+        time_slots: v
+      }
+    end
+
+    render json: result, status: :ok
+  end
+
   def create
     today = Time.zone.now.strftime("%Y-%m-%d")
     appointment = Appointment.where(user_id: appointment_params[:user_id], date: today).first
--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Appointments", :type => :request do
+
+  describe "GET /appointments" do
+    it "returns all appointments from today grouped by place and time as json" do
+      Appointment.create(user_id: 1, place_id: 1, time: "12:00", date: Time.zone.now)
+      Appointment.create(user_id: 2, place_id: 1, time: "12:00", date: Time.zone.now)
+      Appointment.create(user_id: 3, place_id: 1, time: "12:15", date: Time.zone.now)
+      Appointment.create(user_id: 4, place_id: 2, time: "12:15", date: Time.zone.now)
+
+      get appointments_path
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json[0]["place_id"]).to eq(1)
+      expect(json[1]["place_id"]).to eq(2)
+      expect(json[0]["time_slots"]["12:00"]).to eq([1, 2])
+      expect(json[0]["time_slots"]["12:15"]).to eq([3])
+      expect(json[1]["time_slots"]["12:15"]).to eq([4])
+    end
+  end
+
   describe "POST /appointments" do
     it "adds an appointment" do
       expect {
