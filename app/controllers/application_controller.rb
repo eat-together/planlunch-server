aa6eee10e620339e444ad2e9e692669e0d222aee
pogopaule <pogopaule@gmx.de>
2015-02-17 21:18:22 +0100

    enable cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+
+  after_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
 end
