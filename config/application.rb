926d5f572e5a0daf22c1c208b592490daa474e75
pogopaule <pogopaule@gmx.de>
2015-02-20 14:43:16 +0100

    add delete http vert to cors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module RailsServer
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
       end
     end
   end
